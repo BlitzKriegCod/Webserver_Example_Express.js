924392ef9529e1290d3aebb35ea9988962fc53a9
BlitzKriegCod <yuleskygonzalez534@gmail.com>
2024-07-25 12:17:29 -0400

    Second

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express() 
-const port = 8080
+require('dotenv').config()
+const port = process.env.PORT
+
 const hbs = require('hbs');
 
 //set server port
@@ -8,10 +10,18 @@ app.listen(port,()=>{console.log("App runing on port:",port)})
 
 //hbs config
 app.set('view engine','hbs')
-hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+hbs.registerPartials(__dirname + '/views/partials', function (err) {
+   if(err) console.log("Ooops partials", err ,' !!!')
+});
 //static supplies
 app.use(express.static('public'));
 //basic routing
 app.get('/',(req,res)=>{
     res.render('home')
+})
+app.get('/elements',(req,res)=>{
+    res.render('elements')
+})
+app.get('/generic',(req,res)=>{
+    res.render('generic')
 })
